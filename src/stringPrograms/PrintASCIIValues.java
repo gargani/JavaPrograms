74a99fbcdd1947c13192146e5594b69406672a8a
Anirudh Garg <anirudh.garg92@gmail.com>
2020-10-11 00:40:25 +0530

    Adding Java Programs

---
+++ src/stringPrograms/PrintASCIIValues.java
@@ -0,0 +1,13 @@
+package stringPrograms;
+
+public class PrintASCIIValues {
+
+    public static void PrintASCIIValue() {
+
+        for (int i = 0; i <= 255; i++) {
+            System.out.println(i + " " + (char) i);
+        }
+        char i = '2';
+        int a = i;
+    }
+}
---
+++ src/stringPrograms/ToCheckStringIsAnagram.java
@@ -0,0 +1,35 @@
+package stringPrograms;
+import java.util.Arrays;
+
+public class ToCheckStringIsAnagram {
+    public static void CheckAnagram() {
+
+        String str1 = "Listen";
+        String str2 = "Silent";
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        boolean flag = true;
+
+        flag = str1.length() == str2.length();
+
+        if (flag) {
+            char[] c1 = str1.toCharArray();
+            char[] c2 = str2.toCharArray();
+
+            Arrays.sort(c1);
+            Arrays.sort(c2);
+            // flag = Arrays.equals(c1, c2);
+
+            for (int i = 0; i < str1.length(); i++)
+                if (c1[i] != c2[i])
+                    flag = false;
+        }
+
+        if (flag)
+            System.out.println("Strings are Anagram");
+        else
+            System.out.println("Strings are not Anagram");
+
+    }
+}
